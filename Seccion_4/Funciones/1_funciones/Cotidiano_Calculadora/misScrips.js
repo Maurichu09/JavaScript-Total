715dd2659f7eeff3e28088c6d51adac8681e2469
Maurichu09 <maurisecundario2@gmail.com>
2025-03-14 15:59:23 -0600

    Calculadora

---
+++ Seccion_4/Funciones/1_funciones/Cotidiano_Calculadora/misScrips.js
@@ -0,0 +1,33 @@
+function suma(){
+
+}
+function resta(){
+
+}
+function multiplicar(){
+
+}
+function division(){
+
+}
+function Raiz(){
+
+}
+function potencia(){
+
+}
+function absoluto(){
+
+}
+function random(){
+
+}
+function round(){
+
+}
+function floor(){
+
+}
+function ceil(){
+    
+}
