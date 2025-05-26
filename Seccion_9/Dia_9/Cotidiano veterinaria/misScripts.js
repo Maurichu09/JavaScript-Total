91c5bdb9be2eb702ff00cd31859cc12d6bb53d61
Maurichu09 <maurisecundario2@gmail.com>
2025-05-26 16:01:03 -0600

    Cotidiano animales

--- Seccion_9/Dia_9/Cotidiano veterinaria/misScripts.js
+++ Seccion_9/Dia_9/Cotidiano veterinaria/misScripts.js
@@ -1,41 +1,59 @@
-class perro{
-    constructor(nombre, peso, raza){
+class animalitos {
+    constructor(nombre, peso, edad) {
         this.nombre = nombre;
         this.peso = peso;
+        this.edad = edad;
+    }
+    informacion() {
+        return `Nombre: ${this.nombre}, peso: ${this.peso}, edad: ${this.edad}`;
+    }
+}
+
+class perro extends animalitos {
+    constructor(nombre, peso, raza, edad) {
+        super(nombre, peso, edad);
         this.raza = raza;
     }
+    informacion() {
+        return `El nombre es: ${this.nombre}, peso: ${this.peso}, la raza es: ${this.raza} y su edad es ${this.edad}`;
+    }
 }
-let perrito = new perro("Tuky", "4kg", "chihuahua");
 
-class gato{
-    constructor(nombre, peso, sexo){
-        this.nombre = nombre;
-        this.peso = peso;
+class gato extends animalitos {
+    constructor(nombre, peso, sexo, edad) {
+        super(nombre, peso, edad);
         this.sexo = sexo;
     }
-}
-let gatito = new gato("Frijol", "3kg", "Masculino");
-
-class conejo{
-    constructor(nombre, peso, color){
-        this.nombre = nombre;
-        this.peso = peso;
-        this.color = color;
+    informacion() {
+        return `El nombre es: ${this.nombre}, peso: ${this.peso}, el sexo es: ${this.sexo} y su edad es ${this.edad}`;
     }
 }
-let conejito = new conejo("Mike", "4kg", "blanco");
 
-let elementoRespuesta = document.getElementById("listaMascotas");
-let myArray = [perrito, gatito, conejito]
+class conejo extends animalitos {
+    constructor(nombre, peso, color, edad) {
+        super(nombre, peso, edad);
+        this.color = color;
+    }
+    informacion() {
+        return `El nombre es: ${this.nombre}, peso: ${this.peso}, el color es: ${this.color} y su edad es ${this.edad}`;
+    }
+}
 
-function verMascotas(){
-    let Lista = document.getElementById("listaMascotas");
-    let elementosLi = Lista.getElementsByTagName("li");
+let perrito = new perro("Tuky", "4kg", "Chihuahua", "10 anios");
+let conejito = new conejo("Bugs", "5kg", "Gris", "2 anios");
+let gatito = new gato("Kitty", "4kg", "hembra", "1 mes");
+let myArray = [perrito, gatito, conejito];
 
-    let primerElemento = elementosLi[0];
 
-    primerElemento.textContent = myArray;
+document.getElementById('showAnimalsBtn').addEventListener('click', verMascotas);
 
-    let nuevoElemento = document.createElement("li");
-    Lista.appendChild(nuevoElemento);
+function verMascotas() {
+    const animalsContainer = document.getElementById('listaMascotas');
+    animalsContainer.innerHTML = '';
+    myArray.forEach(animal => {
+        const animalCard = document.createElement('div');
+        animalCard.classList.add('animal-card');
+        animalCard.textContent = animal.informacion();
+        animalsContainer.appendChild(animalCard);
+    });
 }
